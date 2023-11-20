4c62e9d60e30612f09076477a4a797dcdd6f7f15
Hiraoka-Kazuya <hiraoka.kazuya@email.com>
2023-11-21 00:34:19 +0900

    topページ、レスポシブデザイン適応業中

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 for (let item of __item_data) {
   const listItem = document.createElement('div')
-  // innerHTMLにて文字列でHTMLタグを生成することができます。
-  // ただし多少くせがあります。
-  console.log(item)
   listItem.innerHTML = `
-    
-      <img src="${item.image}" />
-      <div>${item.itemName}</div>
-      <div>¥${item.price}</div>
-    
+  <div class="itemList-container">
+    <img src="${item.image}" />
+    <div>${item.itemName}</div>
+    <span>¥${item.price}</span>
+  </div>
   `
   document.querySelector('#list').appendChild(listItem)
 }
