1e9a72fe5b4339f849a4b998854c1f463098ab40
Hiraoka-Kazuya <hiraoka.kazuya@email.com>
2024-02-16 14:31:18 +0900

    detail画面 カートに入れるボタン活性・非活性化　完了

--- detail/detail.js
+++ detail/detail.js
@@ -17,22 +17,22 @@ document.querySelector('.item-image').appendChild(itemImage)
 // アイテム情報
 const detailName = document.createElement('div')
 detailName.innerHTML = `
-<div class="productInformation-container">
+  <div class="productInformation-container">
     <div class="detail-itemName">${itemName}</div>
     <div class="detailedSelection-container">
       <div class="productSize-container">
-        size:
-        <select name="size">
-          <option value="size-emptiness"></option>
+        <label for="selectSize">size:</label>
+        <select name="size" id="selectSize">
+          <option value="size-emptiness" selected></option>
           <option value="size90">90</option>
           <option value="size100">100</option>
           <option value="size110">110</option>
         </select>
       </div>
       <div class="productQuantity-container">
-        quantity:
-        <select name="quantity">
-          <option value="product-0"></option>
+        <label for="selectQuantity">quantity:</label>
+        <select name="quantity" id="selectQuantity">
+          <option value="product-0" selected></option>
           <option value="product-1">1</option>
           <option value="product-2">2</option>
           <option value="product-3">3</option>
@@ -40,9 +40,36 @@ detailName.innerHTML = `
       </div>
     </div>
     <div>¥ ${price}（税込）</div>
-    <button type="button" id="addToCartButton">カートに入れる</button>    
+    <button type="button" id="addToCartButton" disabled="true">カートに入れる</button>
   </div>
 `
 document.querySelector('.itemDetail-container').appendChild(detailName)
 
-// アイテム金額
+// アイテムsize & アイテムquantity 入力後のボタンの活性化
+function activityStatus() {
+  const sizeElement = document.getElementById('selectSize').value
+  console.log(sizeElement, typeof sizeElement)
+  const quantityElement = document.getElementById('selectQuantity').value
+  console.log(quantityElement, typeof quantityElement)
+  if (quantityElement !== 'product-0' && sizeElement !== 'size-emptiness') {
+    console.log('サイズと個数選択')
+    document.querySelector('#addToCartButton').disabled = false
+  }
+// quantityElement.onchange = function(){
+//   console.log(this.value);
+//   if (this.value > 0) {
+//     console.log('true')
+//   }
+// }
+
+// sizeElement.onchange = function(){
+//   console.log(this.value);
+// }
+}
+
+document.querySelector('#selectSize')
+.addEventListener('change', activityStatus)
+
+document.querySelector('#selectQuantity')
+.addEventListener('change', activityStatus)
+
